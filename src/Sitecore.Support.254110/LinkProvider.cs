2cea19e28474b7c1a8d1a8e18ef4c20a1f4809e9
Giacomo Portolan <gpo@sitecore.net>
2018-09-14 10:01:27 -0400

    Commit changes for patch

---
+++ src/Sitecore.Support.254110/LinkProvider.cs
@@ -0,0 +1,41 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Links;
+using System;
+
+namespace Sitecore.Support.Links
+{
+  public class LinkProvider : Sitecore.Links.LinkProvider
+  {
+    public new class LinkParser : Sitecore.Links.LinkProvider.LinkParser
+    {
+      public override string ExpandDynamicLinks(string text, UrlOptions options)
+      {
+        Assert.ArgumentNotNull(text, "text");
+        Assert.ArgumentNotNull(options, "options");
+        if (string.IsNullOrEmpty(text))
+        {
+          return string.Empty;
+        }
+
+        foreach (LinkExpander expander in GetExpanders())
+        {
+          try
+          {
+            expander.Expand(ref text, options);
+          }
+          catch (Exception e)
+          {
+            Log.Error("Exception occourred while parsing link", e, this);
+          }
+        }
+
+        return text;
+      }
+    }
+
+    protected override Sitecore.Links.LinkProvider.LinkParser CreateLinkParser()
+    {
+      return new LinkParser();
+    }
+  }
+}
---
+++ src/Sitecore.Support.254110/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.254110")]
+[assembly: AssemblyProduct("Sitecore.Support.254110")]
+[assembly: ComVisible(false)]
